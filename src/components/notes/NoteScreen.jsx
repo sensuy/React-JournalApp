9cbeb694cba31a9250b7dc0a1bc240f46e49cb0d
sensui <sensuijunior@hotmail.com>
2020-10-17 21:24:02 -0300

    init redux

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
 	return (
 		<div className="notes__main-content">
 			<NotesAppBar />
+			<div className="notes__content">
+				<input
+					type="text"
+					placeholder="some awesome title"
+					className="notes__title-input"
+				/>
+
+				<textarea
+					placeholder="What happened today?"
+					className="notes__textarea"
+				>
+				</textarea>
+				<div className="notes__image">
+					<img
+						alt="imagen"
+						src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+					/>
+				</div>
+
+			</div>
 		</div>
 	)
 }
